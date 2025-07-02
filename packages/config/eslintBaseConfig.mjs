31ab99cfefdb951f9c439079e1b39dd738cac779
Fabio Colella <fcole90@gmail.com>
2025-07-02 15:30:44 +0200

    Update linting

--- packages/config/eslintBaseConfig.mjs
+++ packages/config/eslintBaseConfig.mjs
@@ -1,3 +1,38 @@
 // @ts-check
 
-export const eslintBaseConfig = [];
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+
+export const eslintBaseConfig = tseslint.config(
+  eslint.configs.recommended,
+  eslintPluginPrettierRecommended,
+  ...tseslint.configs.strictTypeChecked,
+  {
+    rules: {
+      // Note: you must disable the base rule as it can report incorrect errors
+      curly: ['error', 'all'],
+      "no-useless-rename": "error",
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          "args": "all",
+          "argsIgnorePattern": "^_",
+          "caughtErrors": "all",
+          "caughtErrorsIgnorePattern": "^_",
+          "destructuredArrayIgnorePattern": "^_",
+          "varsIgnorePattern": "^_",
+          "ignoreRestSiblings": true
+        }
+      ]
+    },
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    ignores: ['node_modules', 'dist'],
+  },
+);
