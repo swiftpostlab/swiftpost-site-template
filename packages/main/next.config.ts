19457577d2b151c170049e24dfb0dc0885bfec7c
Fabio Colella <fcole90@gmail.com>
2025-07-04 16:50:32 +0200

    Fix basePath on local

--- packages/main/next.config.ts
+++ packages/main/next.config.ts
@@ -1,8 +1,9 @@
 import type { NextConfig } from 'next';
+const isDev = process.env.NODE_ENV === 'development';
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/swiftpost-next-static-template',
+  basePath: isDev ? '' : '/swiftpost-next-static-template',
 };
 
 export default nextConfig;
